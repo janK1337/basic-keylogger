ed5e5dd7d9d9bd8689ca6baab1425d54199a88b3
Pawelekk <49805256+notPawelekk@users.noreply.github.com>
2020-10-13 17:40:07 +0200

    Add files via upload

---
+++ example.js
@@ -0,0 +1,3 @@
+const listener = require("./listener")
+
+new listener(/*Console log delay loop (ms) default: 3000ms (3s)*/)
---
+++ listener.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const ioHook = require("iohook");
+
+class keypress {
+
+    static fromCharCode(num) {
+        if(num <= 31) {
+            num = "Control character";
+            return num;
+        }
+        if(num == 32) {
+            num = "Space";
+            return num;
+        }
+        num = String.fromCharCode(num);
+        return num;
+    }
+}
+class listener {
+
+    constructor(interval) {  
+        let pressedkeys = [];
+        this.interval = isNaN(interval) ? 3000 : interval;
+       
+        ioHook.on('keypress', event => {
+            pressedkeys.push(keypress.fromCharCode(event.keychar)); 
+        })
+        setInterval(function() {
+            console.log(pressedkeys);
+            pressedkeys = []
+        }, this.interval)
+        ioHook.start(); 
+    }
+   
+} 
+module.exports = listener
+
+
+
